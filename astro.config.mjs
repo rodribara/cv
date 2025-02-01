7e84beb116e3deb1340ad8b510540ac8ceb4a555
rodrigo <lairaravena@gmail.com>
2025-02-01 00:20:02 -0300

    fixed issues with relative imports

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,5 +5,14 @@ import tailwind from '@astrojs/tailwind';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()]
+  integrations: [tailwind()],
+  vite: {
+    resolve: {
+      alias: {
+        '@components': '/src/components',
+        '@assets': '/public/assets',
+        '@data': '/src/data'
+      }
+    }
+  }
 });
