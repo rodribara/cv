fba8b2947505e0573935894fe36e28ccb2cc5aa7
rodrigo <lairaravena@gmail.com>
2025-02-14 10:35:55 -0300

    added basic cube

---
+++ src/components/Cube.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+import { useGLTF } from "@tresjs/cientos";
+
+const { nodes } = await useGLTF("/models/Cube.glb", { draco: true });
+const model = nodes.Cube;
+
+
+</script>
+
+<template>
+    <primitive :object="model" />
+</template>
---
+++ src/components/ThreeSection.vue
@@ -0,0 +1,47 @@
+<script setup lang="ts">
+import { OrbitControls, Stars } from '@tresjs/cientos'
+import { TresCanvas } from '@tresjs/core'
+import { BasicShadowMap, SRGBColorSpace, NoToneMapping } from 'three'
+import Cube from './Cube.vue'
+
+const gl = {
+    clearColor: "#181C3E",
+    shadows: true,
+    alpha: true,
+    shadowMapType: BasicShadowMap,
+    outputColorSpace: SRGBColorSpace,
+    toneMapping: NoToneMapping,
+}
+</script>
+<template>
+    <div>Hello World</div>
+    <div class="tres-container">
+        Hello
+
+        <TresCanvas v-bind="gl">
+            <TresPerspectiveCamera :position="[0, 5, 5]" :look-at="[0, 1, 1]" />
+            <Stars />
+            <OrbitControls />
+            <Suspense>
+                <Cube />
+            </Suspense>
+            <TresAmbientLight :color="0xffffff" :intensity="1.5" />
+            <TresDirectionalLight :color="0xffffff" :intensity="1" :position="[-4, -2, 2]" cast-shadow />
+            <TresDirectionalLight :color="0xffffff" :intensity="2" :position="[4, 6, 4]" />
+
+        </TresCanvas>
+    </div>
+</template>
+
+<style>
+.tres-container {
+    width: 100%;
+    height: 100%;
+    background-color: red;
+}
+
+canvas {
+    width: 100%;
+    height: 100%;
+}
+</style>
