8ee679d5f92d6abaea5a430e9dfa85edd96d19f0
rodrigo <lairaravena@gmail.com>
2025-12-09 00:56:07 -0300

    redesigned cv

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,13 +1,15 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {colors: {
-			darkBlue: '#26273f',
-			pinkish: "#fe5980",
-			damascus: "#FF6464"
-			
-		  },},
-	},
-	plugins: [],
-}
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    extend: {
+      colors: {
+        darkBlue: "#26273f",
+        pinkish: "#fe5980",
+        damascus: "#FF6464",
+        onyx: "#373737",
+      },
+    },
+  },
+  plugins: [],
+};
